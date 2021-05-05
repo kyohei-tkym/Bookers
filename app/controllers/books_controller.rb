25868abb4ea22ac5fcca3165ea1f2e2745a50018
kyohei-tkym <kyohe1208@gmail.com>
2021-05-05 01:58:03 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
   end
 
   def edit
@@ -22,12 +23,14 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully update."
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroy."
   end
 
   private
